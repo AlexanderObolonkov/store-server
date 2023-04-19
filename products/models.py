d201ec22a3719104d9663d59f048fd3866f8fe2e
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-04-19 21:05:18 +0300

    Dunder str for ProductCategory model

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class ProductsCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
