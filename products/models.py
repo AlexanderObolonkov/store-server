da0d7ecdd03f762bd222e9d68732674364064202
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-04-19 19:03:44 +0300

    Models added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductsCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(ProductsCategory, on_delete=models.PROTECT)
