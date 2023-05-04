b575ae7b3a69911e63cca570dd467bc5e30d7503
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-05-04 21:52:03 +0300

    Form experiments

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from users.models import User
+from users.forms import UserLoginForm
+
 
 def login(request: HttpRequest) -> HttpResponse:
-    return render(request, 'users/login.html')
+    context = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', context)
 
 
 def registration(request: HttpRequest) -> HttpResponse:
