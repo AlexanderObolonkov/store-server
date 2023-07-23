05392be3fa21506bf2f7f74bf9f4ea50ba74f31a
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-07-23 22:22:43 +0300

    Stripe payment integration, part 2: stripe price id integration to Product model, webhook setup

--- orders/views.py
+++ orders/views.py
@@ -4,11 +4,13 @@ import stripe
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
+from products.models import Basket
 
 stripe.api_key = settings.STRIPE_PRIVATE_KEY
 
@@ -21,16 +23,14 @@ class OrderCreteView(TitleMixin, CreateView):
 
     def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         super().post(request, *args, **kwargs)
+        baskets = Basket.objects.filter(user=self.request.user)
+
         checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    'price': 'price_1NX2kYLXH8JXaLOAqMQQeW1X',
-                    'quantity': 1,
-                },
-            ],
+            line_items=baskets.stripe_products(),
+            metadata={'order_id': self.object.id},
             mode='payment',
-            success_url=f'{settings.DOMAIN_NAME}{reverse("orders:order_success")}',
-            cancel_url=f'{settings.DOMAIN_NAME}{reverse("orders:order_canceled")}',
+            success_url=f'{settings.DOMAIN_IP_NAME}{reverse("orders:order_success")}',
+            cancel_url=f'{settings.DOMAIN_IP_NAME}{reverse("orders:order_canceled")}',
         )
         return HttpResponseRedirect(checkout_session.url, status=HTTPStatus.SEE_OTHER)
 
@@ -46,3 +46,37 @@ class SuccessTemplateView(TitleMixin, TemplateView):
 
 class CanceledTemplateView(TitleMixin, TemplateView):
     template_name = 'orders/canceled.html'
+
+
+@csrf_exempt
+def stripe_webhook_view(request: HttpRequest) -> HttpResponse:
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError:
+        # Invalid payload
+        return HttpResponse(status=HTTPStatus.BAD_REQUEST)
+    except stripe.error.SignatureVerificationError:
+        # Invalid signature
+        return HttpResponse(status=HTTPStatus.BAD_REQUEST)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        # Fulfill the purchase...
+        fulfill_order(session)
+
+    # Passed signature verification
+    return HttpResponse(status=HTTPStatus.OK)
+
+
+def fulfill_order(session) -> None:
+    # TODO: filling, chilling
+    # order_id = int(session.metadata.order_id)
+    print("Fulfilling order")
--- products/models.py
+++ products/models.py
@@ -1,7 +1,11 @@
+import stripe
 from _decimal import Decimal
+from django.conf import settings
 from django.db import models
 
 from users.models import User
+
+stripe.api_key = settings.STRIPE_PRIVATE_KEY
 
 
 class ProductsCategory(models.Model):
@@ -22,6 +26,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=9, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
+    stripe_product_price_id = models.CharField(max_length=128, null=True, blank=True)
     category = models.ForeignKey(ProductsCategory, on_delete=models.PROTECT)
 
     class Meta:
@@ -31,6 +36,25 @@ class Product(models.Model):
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category}'
 
+    def save(
+            self, force_insert: bool = False, force_update: bool = False, using: bool = None, update_fields: bool = None
+    ) -> None:
+        if not self.stripe_product_price_id:
+            stripe_product_price = self.create_stripe_product_price()
+            self.stripe_product_price_id = stripe_product_price['id']
+        super().save(force_insert, force_update, using, update_fields)
+
+    def create_stripe_product_price(self) -> stripe.Price:
+        kopecks_in_rubles = 100
+
+        stripe_product = stripe.Product.create(name=self.name)
+        stripe_product_price = stripe.Price.create(
+            product=stripe_product['id'],
+            unit_amount=round(self.price * kopecks_in_rubles),
+            currency='rub'
+        )
+        return stripe_product_price
+
 
 class BasketQuerySet(models.QuerySet):
     def total_sum(self) -> Decimal:
@@ -38,6 +62,12 @@ class BasketQuerySet(models.QuerySet):
 
     def total_quantity(self) -> int:
         return sum(basket.quantity for basket in self)
+
+    def stripe_products(self) -> list[dict[str, int]]:
+        line_items = [{'price': basket.product.stripe_product_price_id,
+                       'quantity': basket.quantity}
+                      for basket in self]
+        return line_items
 
 
 class Basket(models.Model):
